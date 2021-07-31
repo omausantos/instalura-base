e2478df059d5343f1a7af65fc8a6e0753a435450
Maurício Santos <mauwebjc@gmail.com>
2021-07-31 13:15:42 -0300

    [Aula 09] Criando o componente Grid com Styled Components
    Ajustes extras:
    - simplificar breakPoint CSS no componente Grid.Col

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Menu from '../src/components/commons/Menu'
 import Footer from '../src/components/commons/Footer'
 import Text from '../src/components/foundation/Text'
 import { Button } from '../src/components/commons/Button'
+import { Grid } from '../src/components/foundation/Grid'
 
 export default function Home() {
   return (
@@ -14,42 +15,60 @@ export default function Home() {
     }}>
       <Menu />
 
-      <div>
-        <Text
-          variant="title"
-          tag="h1"
-          color="tertiary.main"
-          textAlign={{
-            xs: 'center',
-            md: 'left',
-          }}
-        >
-          Compartilhe momentos e conecte-se com amigos
-        </Text>
-        <Text
-          variant="paragraph1"
-          tag="p"
-          color="tertiary.light"
-          textAlign={{
-            xs: 'center',
-            md: 'left',
-          }}
-        >
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-          Lorem Ipsum has been the industrys standard dummy text ever since the 1500s.
-        </Text>
+      <Grid.Container>
+        <Grid.Row>
+          <Grid.Col
+            offset={{ xs: 0, md: 1 }}
+            col={{ xs: 12, md: 5 }}
+          >
+            <div>
+              <Text
+                variant="title"
+                tag="h1"
+                color="tertiary.main"
+                textAlign={{
+                  xs: 'center',
+                  md: 'left',
+                }}
+              >
+                Compartilhe momentos e conecte-se com amigos
+              </Text>
+              <Text
+                variant="paragraph1"
+                tag="p"
+                color="tertiary.light"
+                textAlign={{
+                  xs: 'center',
+                  md: 'left',
+                }}
+              >
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                Lorem Ipsum has been the industrys standard dummy text ever since the 1500s.
+              </Text>
 
-        <Button
-          variant="primary.main"
-          display="block"
-          margin={{
-            xs: 'auto',
-            md: 'initial',
-          }}
-        >
-          Cadastrar
-        </Button>
-      </div>
+              <Button
+                variant="primary.main"
+                display="block"
+                margin={{
+                  xs: 'auto',
+                  md: 'initial',
+                }}
+              >
+                Cadastrar
+              </Button>
+            </div>
+          </Grid.Col>
+          <Grid.Col
+            col={{ xs: 12, md: 6 }}
+          >
+            <img
+              style={{ display: 'block', margin: 'auto' }}
+              src="https://bootcamp-alura-01-git-modulo01.omariosouto.vercel.app/images/phones.png"
+            />
+          </Grid.Col>
+        </Grid.Row>
+      </Grid.Container>
+
 
       <Footer />
     </div>
---
+++ src/components/foundation/Grid/index.js
@@ -0,0 +1,89 @@
+import styled, { css } from "styled-components";
+import { breakpointsMedia } from "../../../theme/utils/breakpointsMedia";
+
+const Container = styled.div`
+    padding-left: 28px;
+    padding-right: 28px;
+    margin-left: auto;
+    margin-right: auto;
+    width: 100%;
+    ${breakpointsMedia({
+        sm: css`
+            max-width: 576px; 
+        `,
+        md: css`
+            max-width: 768px;
+            padding-right: 16px;
+            padding-left: 16px; 
+        `,
+        lg: css`
+            max-width: 1160px; 
+        `,
+        xl: css`
+            max-width: 1222px;
+        `,
+    })};
+`;
+
+const Row = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    margin-right: -16px;
+    margin-left: -16px;
+`;
+
+const Col = styled.div`
+    padding-right: 16px;
+    padding-left: 16px;    
+    max-width: 100%;
+    flex-basis: 0;
+    flex-grow: 1;
+    ${function({col}){
+        if (typeof col === 'number') {
+            return css`
+                flex: 0 0 ${(100 * col) / 12}%;
+                max-width: ${(100 * col) / 12}%;
+            `;
+        }
+        
+        const breakPoints = Object.keys(col);
+        return breakPoints.map((breakPoint) => 
+            breakpointsMedia({
+                [breakPoint]: css`
+                    flex: 0 0 ${(100 * col[breakPoint]) / 12}%;
+                    max-width: ${(100 * col[breakPoint]) / 12}%;
+                `,
+            })            
+        )
+
+    }};
+    
+    ${function({offset}){
+        if (typeof offset === 'number') {
+            return css`
+                margin-left: ${(100 * offset) / 12}%;
+            `;
+        }
+        
+        const breakPoints = Object.keys(offset);
+        return breakPoints.map((breakPoint) => 
+            breakpointsMedia({
+                [breakPoint]: css`
+                    margin-left: ${(100 * offset[breakPoint]) / 12}%;
+                `,
+            })            
+        )
+
+    }};
+`;
+
+export const Grid = {
+    Container,
+    Row,
+    Col,
+}
+
+Col.defaultProps = {
+    col: {},
+    offset: {},
+}
