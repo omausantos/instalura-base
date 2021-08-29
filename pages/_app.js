88605ad940e50603e45d456b6635a6d0368d26fa
Maurício Santos <mauwebjc@gmail.com>
2021-08-29 16:23:42 -0300

    feat(seo): criação da função para gerenciamento das SEO Tags
    - inclusão como dependencia da página home

--- pages/_app.js
+++ pages/_app.js
@@ -3,19 +3,14 @@ import { ThemeProvider } from 'styled-components';
 import Head from 'next/head';
 import theme from '../src/theme';
 import GlobalStyle from '../src/theme/GlobalStyle';
+import { SEO } from '../src/components/commons/SEO';
 
 // eslint-disable-next-line react/prop-types
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <SEO headTitle="Home" />
       <Head>
-        <title>Instalura - Vida eterna ao Mestre Nicolas Cage</title>
-        <meta name="description" content="Uma simples homenagem ao melhor ator do mundo. Vida eterna ao Mestre Nicolas Cage!" />
-        <meta property="og:title" content="Instalura" />
-        <meta property="og:description" content="Uma simples homenagem ao melhor ator do mundo. Vida eterna ao Mestre Nicolas Cage!" />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://instalura-base-omausantos.vercel.app/" />
-        <meta property="og:image" content="https://instalura-base-omausantos.vercel.app/images/projeto-base.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" />
