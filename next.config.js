46baf6860406b49c6eeba2166f1ec2939e18aac7
Maurício Santos <mauwebjc@gmail.com>
2021-09-02 22:01:21 -0300

    feat(nextconfig): inclusão de redirect para /app/login/

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   trailingSlash: true,
+  async redirects() {
+    return [
+      {
+        source: '/login/',
+        destination: '/app/login/',
+        permanent: true,
+      },
+    ];
+  },
 };
