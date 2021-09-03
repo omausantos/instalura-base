ba00c6f1e2314efa8f43ae6fee4ca9d459ad5332
Maurício Santos <mauwebjc@gmail.com>
2021-09-02 22:06:10 -0300

    feat(nextconfig): separando redirects em arquivo apartado

--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
+const redirect = require('./config/redirects');
+
 module.exports = {
   trailingSlash: true,
   async redirects() {
-    return [
-      {
-        source: '/login/',
-        destination: '/app/login/',
-        permanent: true,
-      },
-    ];
+    return redirect;
   },
 };
